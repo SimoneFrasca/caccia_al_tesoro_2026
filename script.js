acbc88d0b8f3a961129d7c29066ef413452c1e14
SimoneFrasca <simone.frasca.99@gmail.com>
2026-08-20 07:02:47 +0200

    v8

--- script.js
+++ script.js
@@ -5,156 +5,157 @@ const CONFIG_LIVELLI = {
   livello1: {
     risposte: { chiave_livello: "859317" },
     indizi: [
-      { nome: " > dove potete trovare le notizie?", tempoAttesaMs: 10 * secondi },
-      { nome: " > un indizio è nelle stelle del nostro mese", tempoAttesaMs: 5 * secondi },
-      { nome: " > avete diviso la verità per far combaciare le stranezze?", tempoAttesaMs: 5 * secondi },
-      { nome: " > dove cammina il treno?", tempoAttesaMs: 5 * secondi },
-      { nome: " > doppio bianco è spazio", tempoAttesaMs: 5 * secondi },
-      { nome: " > nero sopra bianco sotto -> 1", tempoAttesaMs: 5 * secondi },
-      { nome: " > nero sotto bianco sopra -> 0", tempoAttesaMs: 5 * secondi },
-      { nome: " > 6 cifre", tempoAttesaMs: 5 * secondi },
-      { nome: " > prima e ultima cifra: 8 e 7", tempoAttesaMs: 5 * secondi },
-      { nome: " > terza e quarta cifra: 9 e 3", tempoAttesaMs: 5 * secondi },
-      { nome: " > seconda e quinta cifra: 5 e 1", tempoAttesaMs: 5 * secondi }
+      { nome: " > dove potete trovare le notizie?", tempoAttesaMs: 10 * minuti },
+      { nome: " > un indizio è nelle stelle del nostro mese", tempoAttesaMs: 5 * minuti },
+      { nome: " > avete diviso la verità per far combaciare le stranezze?", tempoAttesaMs: 5 * minuti },
+      { nome: " > dove cammina il treno?", tempoAttesaMs: 5 * minuti },
+      { nome: " > doppio bianco è spazio", tempoAttesaMs: 5 * minuti },
+      { nome: " > nero sopra bianco sotto -> 1", tempoAttesaMs: 5 * minuti },
+      { nome: " > nero sotto bianco sopra -> 0", tempoAttesaMs: 5 * minuti },
+      { nome: " > 6 cifre", tempoAttesaMs: 5 * minuti },
+      { nome: " > prima e ultima cifra: 8 e 7", tempoAttesaMs: 5 * minuti },
+      { nome: " > terza e quarta cifra: 9 e 3", tempoAttesaMs: 5 * minuti },
+      { nome: " > seconda e quinta cifra: 5 e 1", tempoAttesaMs: 5 * minuti }
     ],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 30 * secondi
   },
   livello2: {
     risposte: { chiave_livello: "bottino" },
     indizi: [
-      { nome: " > come si chiama la refurtiva dopo un colpo?", tempoAttesaMs: 10 * secondi },
-      { nome: " > è anche una zona di Affile", tempoAttesaMs: 5 * secondi }
+      { nome: " > come si chiama la refurtiva dopo un colpo?", tempoAttesaMs: 10 * minuti },
+      { nome: " > è anche una zona di Affile", tempoAttesaMs: 5 * minuti }
     ],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 30 * secondi
   },
   livello3: {
     risposte: { chiave_livello: "50" },
     indizi: [
-      { nome: " > Assicuratevi che siano tutti e due", tempoAttesaMs: 5 * secondi },
-      { nome: " > sono di più dei gatti di una canzone", tempoAttesaMs: 3 * secondi },
-      { nome: " > sono meno dei ", tempoAttesaMs: 3 * secondi },
-      { nome: " > sono di più dei gatti di una canzone", tempoAttesaMs: 3 * secondi },
-      { nome: " > sono di meno dei ", tempoAttesaMs: 3 * secondi }
+      { nome: " > Assicuratevi che sia la prima", tempoAttesaMs: 5 * minuti },
+      { nome: " > Assicuratevi di contare sulla piastra", tempoAttesaMs: 5 * minuti },
+      { nome: " > sono di più dei gatti di una canzone", tempoAttesaMs: 3 * minuti },
+      { nome: " > sono meno della metà di 110", tempoAttesaMs: 3 * minuti },
+      { nome: " > sono di più di 6x8", tempoAttesaMs: 3 * minuti },
+      { nome: " > sono di meno di 26x2 ", tempoAttesaMs: 3 * minuti }
     ],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 20 * secondi
   },
   livello4: {
     risposte: { chiave_livello: "59863120" },
     indizi: [
-      { nome: " > non ancora, concentratevi...", tempoAttesaMs: 5 * secondi },
-      { nome: " > dovete penare ancora un po' per avere il primo indizio...", tempoAttesaMs: 5 * secondi },
-      { nome: " > stella rossa = 8", tempoAttesaMs: 3 * secondi },
-      { nome: " > esplosione = 6", tempoAttesaMs: 3 * secondi },
-      { nome: " > quadrato con punto rosso = 1", tempoAttesaMs: 3 * secondi },
-      { nome: " > stella bianca nel cerchio blu = 9", tempoAttesaMs: 3 * secondi }
+      { nome: " > non ancora, concentratevi...", tempoAttesaMs: 10 * minuti },
+      { nome: " > dovete penare ancora un po' per avere il primo indizio...", tempoAttesaMs: 10 * minuti },
+      { nome: " > stella rossa = 8", tempoAttesaMs: 10 * minuti },
+      { nome: " > esplosione = 6", tempoAttesaMs: 10 * minuti },
+      { nome: " > quadrato con punto rosso = 1", tempoAttesaMs: 10 * minuti },
+      { nome: " > stella bianca nel cerchio blu = 9", tempoAttesaMs: 10 * minuti }
     ],
-    tempoBloccoMs: 20 * secondi
+    tempoBloccoMs: 60 * secondi
   },
   livello5: {
     risposte: { chiave_livello: "cooperativa" },
     indizi: [],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 20 * secondi
   },
   livello6: {
     risposte: { chiave_livello: "la libertà che guida il popolo" },
     indizi: [
-      { nome: " > fate attenzione alla richiesta... cosa viene chiesto per prima?", tempoAttesaMs: 5 * secondi },
-      { nome: " > avete trovato il titolo dell'opera (in italiano)?", tempoAttesaMs: 5 * secondi },
-      { nome: " > bisogna inserire il titolo dell'opera", tempoAttesaMs: 5 * secondi }
+      { nome: " > fate attenzione alla richiesta... cosa viene chiesto per prima?", tempoAttesaMs: 10 * minuti },
+      { nome: " > avete trovato il titolo dell'opera (in italiano)?", tempoAttesaMs: 5 * minuti },
+      { nome: " > bisogna inserire il titolo dell'opera", tempoAttesaMs: 5 * minuti }
     ],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 30 * secondi
   },
   livello7: {
     risposte: { chiave_livello: "Leonardo da Vinci" },
     indizi: [
-      { nome: " > qual è la cadenza dei balli?", tempoAttesaMs: 3 * secondi },
-      { nome: " > Y->U", tempoAttesaMs: 2 * secondi },
-      { nome: " > W->T", tempoAttesaMs: 2 * secondi },
-      { nome: " > vale per tutti i pari e dispari", tempoAttesaMs: 2 * secondi },
-      { nome: " > sicuri che la chiave è il luogo? Rileggete la prova precedente per capire cosa dovete inserire", tempoAttesaMs: 2 * secondi },
-      { nome: " > no, non ci siamo. leggete bene la prova precedente, vi chiede prima il titolo (che avete già messo)... poi cosa vi chiede di inserire?", tempoAttesaMs: 2 * secondi },
-      { nome: " > chi è l'artista?", tempoAttesaMs: 2 * secondi }
+      { nome: " > qual è la cadenza dei balli?", tempoAttesaMs: 10 * minuti },
+      { nome: " > Y->U", tempoAttesaMs: 10 * minuti },
+      { nome: " > W->T", tempoAttesaMs: 8 * minuti },
+      { nome: " > vale per tutti i pari e dispari", tempoAttesaMs: 7 * minuti },
+      { nome: " > sicuri che la chiave è il luogo? Rileggete la prova precedente per capire cosa dovete inserire", tempoAttesaMs: 6 * minuti },
+      { nome: " > no, non ci siamo. leggete bene la prova precedente, vi chiede prima il titolo (che avete già messo)... poi cosa vi chiede di inserire?", tempoAttesaMs: 5 * minuti },
+      { nome: " > chi è l'artista?", tempoAttesaMs: 4 * minuti }
     ],
-    tempoBloccoMs: 5 * secondi
+    tempoBloccoMs: 30 * secondi
   },
   livello8: {
     risposte: { chiave_livello: "olio su tela" },
     indizi: [
-      { nome: " > avete fatto bene i conti?", tempoAttesaMs: 4 * secondi },
-      { nome: " > occhio all'indizio", tempoAttesaMs: 3 * secondi },
-      { nome: " > ci interessano tante cifre quante sono le torri di Notre Dame", tempoAttesaMs: 3 * secondi },
-      { nome: " > attenti all'ordine", tempoAttesaMs: 3 * secondi },
-      { nome: " > ricordatevi della prove precedenti, cosa dovete inserire?", tempoAttesaMs: 3 * secondi },
-      { nome: " > se non lo avete ancora capito, avete inserito prima il titolo, poi l'artista... cosa vi manca?", tempoAttesaMs: 3 * secondi },
-      { nome: " > che tecnica è stata utilizzata per l'opera?", tempoAttesaMs: 3 * secondi }
+      { nome: " > avete fatto bene i conti?", tempoAttesaMs: 10 * minuti },
+      { nome: " > occhio all'indizio", tempoAttesaMs: 10 * minuti },
+      { nome: " > ci interessano tante cifre quante sono le torri di Notre Dame", tempoAttesaMs: 10 * minuti },
+      { nome: " > attenti all'ordine", tempoAttesaMs: 5 * minuti },
+      { nome: " > ricordatevi della prove precedenti, cosa dovete inserire?", tempoAttesaMs: 5 * minuti },
+      { nome: " > se non lo avete ancora capito, avete inserito prima il titolo, poi l'artista... cosa vi manca?", tempoAttesaMs: 5 * minuti },
+      { nome: " > che tecnica è stata utilizzata per l'opera?", tempoAttesaMs: 5 * minuti }
     ],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 30 * secondi
   },
   livello9: {
     risposte: { chiave_livello: "591700" },
     indizi: [
-      { nome: " > siete arrivati al termine dell'operazione?", tempoAttesaMs: 2 * secondi },
-      { nome: " > di un'opera d'arte, quale operazione potete fare con due numeri?", tempoAttesaMs: 2 * secondi },
-      { nome: " > quali sono le dimensioni dell'opera?", tempoAttesaMs: 2 * secondi },
-      { nome: " > le avete moltiplicate?", tempoAttesaMs: 2 * secondi }
+      { nome: " > siete arrivati al termine dell'operazione?", tempoAttesaMs: 10 * minuti },
+      { nome: " > di un'opera d'arte, quale operazione potete fare con due numeri?", tempoAttesaMs: 10 * minuti },
+      { nome: " > quali sono le dimensioni dell'opera?", tempoAttesaMs: 6 * minuti },
+      { nome: " > le avete moltiplicate?", tempoAttesaMs: 5 * minuti }
     ],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 30 * secondi
   },
   livello10: {
     risposte: { chiave_livello: "Andres" },
     indizi: [],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 30 * secondi
   },
   livello11: {
     risposte: { chiave_livello: "71481867587186" },
     indizi: [
-      { nome: " > la mappa è un po' troppo vuota... servirebbero dei numeri in quelle caselle", tempoAttesaMs: 5 * secondi },
-      { nome: " > sapete, anche Parigi ha i suoi quartieri", tempoAttesaMs: 5 * secondi },
-      { nome: " > avete notato un particolare in ogni foto?", tempoAttesaMs: 4 * secondi },
-      { nome: " > l'avete vista in giro per Affile?", tempoAttesaMs: 4 * secondi },
-      { nome: " > ad ogni monumento un <i>arrondissement</i>", tempoAttesaMs: 4 * secondi },
-      { nome: " > attenzione, l'ordine è fondamentale", tempoAttesaMs: 4 * secondi },
-      { nome: " > il codice è dato dall'ordine dei quartieri che trovete sulla bici", tempoAttesaMs: 4 * secondi },
-      { nome: " > la foto 8 è la Tour Eiffel che si trova nel 7° arrondissement. Se la prima cifra segnata sulla bici è 8 (corrispondente alla foto 8), vuol dire che in prima posizione del codice va inserto il 7", tempoAttesaMs: 4 * secondi },
-      { nome: " > la foto 3 è l'Arc du Triomphe che si trova nell'8° arrondissement. Se la quarta cifra segnata sulla bici è 3 (corrispondente alla foto 3), vuol dire che in quarta posizione del codice va inserto l'8", tempoAttesaMs: 4 * secondi }
+      { nome: " > la mappa è un po' troppo vuota... servirebbero dei numeri in quelle caselle", tempoAttesaMs: 10 * minuti },
+      { nome: " > sapete, anche Parigi ha i suoi quartieri", tempoAttesaMs: 6 * minuti },
+      { nome: " > avete notato un particolare in ogni foto?", tempoAttesaMs: 4 * minuti },
+      { nome: " > l'avete vista in giro per Affile?", tempoAttesaMs: 4 * minuti },
+      { nome: " > ad ogni monumento un <i>arrondissement</i>", tempoAttesaMs: 4 * minuti },
+      { nome: " > attenzione, l'ordine è fondamentale", tempoAttesaMs: 4 * minuti },
+      { nome: " > il codice è dato dall'ordine dei quartieri che trovete sulla bici", tempoAttesaMs: 4 * minuti },
+      { nome: " > la foto 8 è la Tour Eiffel che si trova nel 7° arrondissement. Se la prima cifra segnata sulla bici è 8 (corrispondente alla foto 8), vuol dire che in prima posizione del codice va inserto il 7", tempoAttesaMs: 4 * minuti },
+      { nome: " > la foto 3 è l'Arc du Triomphe che si trova nell'8° arrondissement. Se la quarta cifra segnata sulla bici è 3 (corrispondente alla foto 3), vuol dire che in quarta posizione del codice va inserto l'8", tempoAttesaMs: 4 * minuti }
     ],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 60 * secondi
   },
   livello12: {
     risposte: { chiave_livello: "56" },
     indizi: [],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 30 * secondi
   },
   livello13: {
     risposte: { chiave_livello: "Rue Brezin" },
     indizi: [
-      { nome: " > si trova nel 14° arrondissement", tempoAttesaMs: 3 * secondi },
-      { nome: " > non è una via principale", tempoAttesaMs: 2 * secondi },
-      { nome: " > è vicino ad una fermata delle metropolitana", tempoAttesaMs: 2 * secondi },
-      { nome: " > è una traversa tra due vie principali", tempoAttesaMs: 2 * secondi },
-      { nome: " > è vicina ad un parco", tempoAttesaMs: 2 * secondi },
-      { nome: " > la via è intitolata ad una persona sepolta al cimitero di Père Lachaise", tempoAttesaMs: 2 * secondi },
-      { nome: " > la via inizia con la lettera B", tempoAttesaMs: 2 * secondi },
-      { nome: " > questa persona era un imprenditore e industriale che visse tra il '700 e l'800", tempoAttesaMs: 2 * secondi },
-      { nome: " > la via termina con la lettera N", tempoAttesaMs: 2 * secondi },
+      { nome: " > si trova nel 14° arrondissement", tempoAttesaMs: 5 * minuti },
+      { nome: " > non è una via principale", tempoAttesaMs: 5 * minuti },
+      { nome: " > è vicino ad una fermata delle metropolitana", tempoAttesaMs: 5 * minuti },
+      { nome: " > è una traversa tra due vie principali", tempoAttesaMs: 5 * minuti },
+      { nome: " > è vicina ad un parco", tempoAttesaMs: 5 * minuti },
+      { nome: " > la via è intitolata ad una persona sepolta al cimitero di Père Lachaise", tempoAttesaMs: 5 * minuti },
+      { nome: " > la via inizia con la lettera B", tempoAttesaMs: 5 * minuti },
+      { nome: " > questa persona era un imprenditore e industriale che visse tra il '700 e l'800", tempoAttesaMs: 5 * minuti },
+      { nome: " > la via termina con la lettera N", tempoAttesaMs: 5 * minuti },
     ],
-    tempoBloccoMs: 1 * secondi
+    tempoBloccoMs: 60 * secondi
   },
   livello14: {
     risposte: { chiave_livello: "4729" }, 
     indizi: [],
-    tempoBloccoMs: 10 * secondi
+    tempoBloccoMs: 60 * secondi
   },
   livello15: {
     risposte: { chiave_livello: "1110326451311412798" }, 
     indizi: [
-      { nome: " > non facciamo il bis (non contate le linee bis)", tempoAttesaMs: 4 * secondi },
-      { nome: " > partiamo dalla linea 11", tempoAttesaMs: 3 * secondi },
-      { nome: " > arriviamo alla linea 8", tempoAttesaMs: 3 * secondi },
-      { nome: " > attenzione ai cambi tra la 2 e la 6 e tra la 4 e la 5", tempoAttesaMs: 3 * secondi },
-      { nome: " > la soluzione è data dalla sequenza delle metro dalla più lenta alla più veloce (senza trattini o spazi)", tempoAttesaMs: 3 * secondi },
+      { nome: " > non facciamo il bis (non contate le linee bis)", tempoAttesaMs: 10 * minuti },
+      { nome: " > partiamo dalla linea 11", tempoAttesaMs: 10 * minuti },
+      { nome: " > arriviamo alla linea 8", tempoAttesaMs: 8 * minuti },
+      { nome: " > attenzione ai cambi tra la 2 e la 6 e tra la 4 e la 5", tempoAttesaMs: 7 * minuti },
+      { nome: " > la soluzione è data dalla sequenza delle metro dalla più lenta alla più veloce (senza trattini o spazi)", tempoAttesaMs: 6 * minuti },
     ],
-    tempoBloccoMs: 1 * secondi
+    tempoBloccoMs: 100 * secondi
   }
 };
 
